1430b1dfeb0d11b1c25ad4d43c99bcac5bdbfc9a
sean <y20120816s@gmail.com>
2026-05-31 19:53:59 +0800

    檔案刪除

--- api/register.php
+++
@@ -1,9 +0,0 @@
-<?php include_once "db.php";
-
-/* $_POST['username']
-$_POST['password']
-$_POST['email']
- */
-
-$sql="INSERT INTO `users` (`username`,`password`,`email`) VALUES('{$_POST['username']}','{$_POST['password']}','{$_POST['email']}')";
-echo $pdo->exec($sql);
